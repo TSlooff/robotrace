2f1ebf6538681c73a5c5c0668022143c25000c92
TSlooff <t.slooff@hotmail.com>
2017-01-18 22:15:50 +0100

    update

--- src/robotrace/BezierTrack.java
+++ src/robotrace/BezierTrack.java
@@ -20,8 +20,8 @@ public class BezierTrack extends RaceTrack {
         t %= 1;
         int NSegment = controlPoints.length/4;
         int currentSegment = (int)Math.floor(t*NSegment)*4;
-        t *= NSegment;
-        t %= 1;
+        t = t * NSegment;
+        t = t % 1;
         return getCubicBezierPnt(t, controlPoints[currentSegment], controlPoints[currentSegment + 1], controlPoints[currentSegment + 2], controlPoints[currentSegment + 3]);
     }
 
@@ -30,15 +30,15 @@ public class BezierTrack extends RaceTrack {
         t %= 1;
         int NSegment = controlPoints.length / 4;
         int currentSegment = (int)Math.floor(t * NSegment)*4;
-        t *= NSegment;
-        t %= 1;
+        t = t * NSegment;
+        t = t % 1;
         return getCubicBezierTng(t, controlPoints[currentSegment], controlPoints[currentSegment + 1], controlPoints[currentSegment + 2], controlPoints[currentSegment + 3]);
 
     }
     
     private Vector getCubicBezierPnt(double t, Vector P0, Vector P1, Vector P2, Vector P3) {
 
-        //P(u)= (1-u)^3 * P0 + 3u(1-u)^2P1 + (1-u)3u^2P2 + u^3P3
+        //P(t)= (1-t)^3 * P0 + 3t(1-t)^2P1 + (1-t)3t^2P2 + t^3P3
         Double x = Math.pow((1 - t), 3) * P0.x + 3 * t * Math.pow((1 - t), 2) * P1.x + 3 * Math.pow(t, 2) * (1 - t) * P2.x + Math.pow(t, 3) * P3.x;
         Double y = Math.pow((1 - t), 3) * P0.y + 3 * t * Math.pow((1 - t), 2) * P1.y + 3 * Math.pow(t, 2) * (1 - t) * P2.y + Math.pow(t, 3) * P3.y;
         Double z = Math.pow((1 - t), 3) * P0.z + 3 * t * Math.pow((1 - t), 2) * P1.z + 3 * Math.pow(t, 2) * (1 - t) * P2.z + Math.pow(t, 3) * P3.z;
@@ -48,7 +48,7 @@ public class BezierTrack extends RaceTrack {
     
     private Vector getCubicBezierTng(double t, Vector P0, Vector P1, Vector P2, Vector P3) {
 
-        //P'(u) = 3 * (1-u)^2 * (P1 - P0) + 6 * (1-u) * u * (P2 - P1) + 3 * u^2 * (P3 - P2)
+        //P'(t) = 3 * (1-t)^2 * (P1 - P0) + 6 * (1-t) * t * (P2 - P1) + 3 * t^2 * (P3 - P2)
         Double x = 3 * Math.pow(1 - t, 2) * (P1.x - P0.x) + 6 * (1 - t) * t * (P2.x - P1.x) + 3 * Math.pow(t, 2) * (P3.x - P2.x);
         Double y = 3 * Math.pow(1 - t, 2) * (P1.y - P0.y) + 6 * (1 - t) * t * (P2.y - P1.y) + 3 * Math.pow(t, 2) * (P3.y - P2.y);
         Double z = 3 * Math.pow(1 - t, 2) * (P1.z - P0.z) + 6 * (1 - t) * t * (P2.z - P1.z) + 3 * Math.pow(t, 2) * (P3.z - P2.z);
